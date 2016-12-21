40e379ce36b57fd068b86dd18170c75e2ebf94fc
xiaoweige <ge.xiaowei@outlook.com>
2016-12-21 10:34:28 +0800

    Relative path in python2.7

---
+++ keras_tutorial/relativepath.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 16:05:31 2016
+
+@author: Stud
+"""
+import os
+
+
+def readFile(filename):
+    filehandle = open(filename)
+    print filehandle.read()
+    filehandle.close()
+
+
+
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+print fileDir
+
+#For accessing the file in the same folder
+filename = "water_demand2009.csv"
+print "This is contents of " +filename
+readFile(filename)
+#
+#For accessing the file in a folder contained in the current folder
+filename = os.path.join(fileDir, './dataset/same.txt')
+#print "This is contents of" +filename
+print filename
+readFile(filename)
+
+#For accessing the file in the parent folder of the current folder
+#filename = os.path.join(fileDir, '..dataset/same.txt')
+#readFile(filename)
+
+#For accessing the file inside a sibling folder.
+filename = os.path.join(fileDir, './folder2/same.txt')
+filename = os.path.abspath(os.path.realpath(filename))
+print filename
+readFile(filename)
